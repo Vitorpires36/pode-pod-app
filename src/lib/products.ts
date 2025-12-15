2e754dbbfb0cef6319b07fb4c8e11a96aa7364a2
Luiz Vitor Pires <62193174+Vitorpires36@users.noreply.github.com>
2025-12-15 16:10:45 -0300

    Add new products and update existing ones

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -2,68 +2,81 @@ import { Product } from "@/types/product";
 
 export const products: Product[] = [
   // IGNITE
+  // ---------------- IGNITE ----------------
+{
+  id: "ignite-v50-5k",
+  name: "IGNITE V50 5K",
+  description: "5.000 PUFFS - Compacto e potente",
+  price: 49.90,
+  image: "/IGNITEV50.png",
+  category: "pod",
+  brand: "Ignite",
+  puffs: "5000",
+  inStock: true,
+  flavors: ["Blue Rasberry Ice", "Strawberry Banana", "Blue Dream", "Strawberry Watermelon", "Strawberry Mango", "Watermelon Ice", "Peach Mango Watermelon"]
+},
+{
+  id: "ignite-v80-8k",
+  name: "IGNITE V80 8K",
+  description: "8.000 PUFFS - Fluxo de ar suave + alta durabilidade",
+  price: 59.90,
+  image: "/IGNITEV80.png",
+  category: "pod",
+  brand: "Ignite",
+  puffs: "8000",
+  inStock: true,
+  flavors: ["Cactus", "Strawberry Ice", "Banana Ice", "Ice Mint", "Passion Fruit Sour Kiwi", "Menthol", "Blueberry Ice"]
+},
+{
+  id: "ignite-v150-15k",
+  name: "IGNITE V150 15K",
+  description: "15.000 PUFFS - Potência e longa duração",
+  price: 109.90,
+  image: "/IGNITEV150.png",
+  category: "pod",
+  brand: "Ignite",
+  puffs: "15000",
+  inStock: true,
+  flavors: ["Ice Mint", "Blue Dream", "Strawberry Apple Watermelon", "Dragon Fruit Lemonade", "Pepermint Cream", "Sour Raspberry", "Watermelon Mix", "Cherry Banana", "Watermelon Ice", "Menthol", "Pineapple Ice"]
+},
   {
-    id: "ignite-v50-5k",
-    name: "IGNITE V50 5K",
-    description: "5.000 PUFFS - Compacto e potente",
-    price: 49.90,
-    image: "/IGNITEV50.png",
-    category: "pod",
-    brand: "Ignite",
-    puffs: "5000",
-    inStock: true,
-    flavors: ["Blue Rasberry Ice", "Strawberry Banana", "Blue Dream", "Strawberry Watermelon", "Strawberry Mango", "Watermelon Ice", "Peach Mango Watermelon"]
-  },
-  {
-    id: "ignite-v80-8k",
-    name: "IGNITE V80 8K",
-    description: "8.000 PUFFS - Fluxo de ar suave + alta durabilidade",
-    price: 59.90,
-    image: "/IGNITEV80.png",
-    category: "pod",
-    brand: "Ignite",
-    puffs: "8000",
-    inStock: true,
-    flavors: ["Cactus", "Strawberry Ice", "Banana Ice", "Ice Mint", "Passion Fruit Sour Kiwi", "Menthol", "Blueberry Ice"]
-  },
-  {
-    id: "ignite-v150-15k",
-    name: "IGNITE V150 15K",
-    description: "15.000 PUFFS - Potência e longa duração",
-    price: 109.90,
-    image: "/IGNITEV150.png",
-    category: "pod",
-    brand: "Ignite",
-    puffs: "15000",
-    inStock: true,
-    flavors: ["Ice Mint", "Blue Dream", "Strawberry Apple Watermelon", "Dragon Fruit Lemonade", "Pepermint Cream", "Sour Raspberry", "Watermelon Mix", "Cherry Banana", "Watermelon Ice", "Menthol", "Pineapple Ice"]
-  },
-  {
-    id: "ignite-v250-25k",
-    name: "IGNITE V250 25K",
-    description: "25.000 PUFFS - Potência e longa duração",
-    price: 109.90,
-    image: "/IGNITEV250.png",
-    category: "pod",
-    brand: "Ignite",
-    puffs: "25000",
-    inStock: true,
-    flavors: ["Ice Mint", "Cactus Lime Soda", "Banana Ice", "Watermelon Ice", "Grape Ice", "Strawberry Kiwi", "Banana Coconut Water", "Blueberry Ice", "Menthol", "Sweet and Sour Pomegranate", "Pineapple Mango", "Strawberry Ice"]
-  },
-  {
-    id: "ignite-v400-40k",
-    name: "IGNITE V400 40K",
-    description: "40.000 PUFFS - Ultra bateria + fluxo ajustável",
-    price: 124.90,
-    image: "/IGNITEV400.png",
-    category: "pod",
-    brand: "Ignite",
-    puffs: "40000",
-    inStock: true,
-    flavors: ["Ice Mint", "Strawberry Ice", "Grape Ice", "Menthol", "Peach Grape", "Watermelon Ice", "Strawberry Watermelon Ice", "Blueberry Ice"]
-  },
-
+  id: "ignite-v250-25k",
+  name: "IGNITE V250 25K",
+  description: "25.000 PUFFS - Potência e longa duração",
+  price: 109.90,
+  image: "/IGNITEV250.png",
+  category: "pod",
+  brand: "Ignite",
+  puffs: "25000",
+  inStock: true,
+  flavors: ["Ice Mint", "Cactus Lime Soda", "Banana Ice", "Watermelon Ice", "Grape Ice", "Strawberry Kiwi", "Banana Coconut Water", "Blueberry Ice", "Menthol", "Sweet and Sour Pomegranate", "Pineapple Mango","Strawberry Ice"]
+},
+{
+  id: "ignite-v400-40k",
+  name: "IGNITE V400 40K",
+  description: "40.000 PUFFS - Ultra bateria + fluxo ajustável",
+  price: 129.90,
+  image: "/IGNITEV400.png",
+  category: "pod",
+  brand: "Ignite",
+  puffs: "40000",
+  inStock: true,
+  flavors: ["Ice Mint", "Strawberry Ice", "Grape Ice", "Menthol", "Peach Grape", "Watermelon Ice", "Strawberry Watermelon Ice", "Blueberry Ice"]
+},
   // ELF BAR
+  {
+    id: "elfbar-ew9000",
+    name: "ELFBAR EW KIT",
+    description: "9.000 PUFFS - Design Premium",
+    price: 79.90,
+    image: "/ELFEW9K.png",
+    category: "pod",
+    brand: "Elf Bar",
+    puffs: "9000",
+    inStock: true,
+    flavors: ["Morango Kiwi", "Mirtilo", "Manga", "Maracujá", "Framboesa"]
+  },
+  
   {
     id: "elfbar-tc10000",
     name: "ELFBAR TOUCH",
@@ -76,46 +89,278 @@ export const products: Product[] = [
     inStock: true,
     flavors: ["Blue Razz Ice", "Strawberry Ice", "Mango Ice"]
   },
+  
   {
-    id: "elfbar-230000",
-    name: "ELFBAR 23K",
-    description: "23.000 PUFFS - Display LED",
+    id: "elfbar-ew-kit-16k",
+    name: "ELFBAR EW KIT",
+    description: "16.000 PUFFS - Sistema POD Recarregável",
     price: 94.90,
-    image: "/ELFBAR23K.png",
+    image: "/ELFEW16K.png",
     category: "pod",
     brand: "Elf Bar",
-    puffs: "23000",
+    puffs: "16000",
     inStock: true,
-    flavors: [/* mantido */]
+    flavors: ["Fresh Mint", "Tropical Fruit", "Berry Mix"]
   },
-  {
-    id: "elfbar-400000",
-    name: "ELFBAR 40K ICE KING",
-    description: "40.000 PUFFS - Display LED",
-    price: 114.90,
-    image: "/ELFBAR40K.png",
+{
+  id: "elfbar-230000",
+  name: "ELFBAR 23K",
+  description: "23.000 PUFFS - Display LED",
+  price: 94.90,
+  image: "/ELFBAR23K.png",
+  category: "pod",
+  brand: "Elf Bar",
+  puffs: "23000",
+  inStock: true,
+  flavors: [
+    "Baja Splash",
+    "Blue Razz Ice",
+    "Blueberry Pear",
+    "Grape Ice",
+    "Green Apple Ice",
+    "Ice Mint",
+    "Kiwi Dragon Fruit",
+    "Lime Grapefruit Ice",
+    "Miami Mint",
+    "Peach Mango Watermelon",
+    "Pineapple Plums Lime Mint",
+    "Sakura Grape",
+    "Spring Mint",
+    "Strawberry Banana",
+    "Strawberry Ice",
+    "Watermelon Ice"
+  ]
+},
+
+ {
+  id: "elfbar-300000",
+  name: "ELFBAR 30K",
+  description: "30.000 PUFFS - Display LED",
+  price: 104.90,
+  image: "/ELFBAR30K.png",
+  category: "pod",
+  brand: "Elf Bar",
+  puffs: "30000",
+  inStock: true,
+  flavors: [
+    "Acai Banana Ice",
+    "Banana Coconut Ice",
+    "Blueberry Sour Raspberry",
+    "Bubbaloo Grape",
+    "Bubbaloo Tutti Frutti",
+    "Cherry Strazz",
+    "Dragon Strawnana",
+    "Elf Love",
+    "Green Apple Ice",
+    "Guava Passion FR Kiwi",
+    "Menthol",
+    "Miami Mint",
+    "Sour Lush Gami",
+    "Spearmint",
+    "Strawberry Ice",
+    "Strawberry Watermelon Ice",
+    "Strawmelon Peach",
+    "Watermelon Ice"
+  ]
+},
+{
+  id: "elfbar-400000",
+  name: "ELFBAR 40K ICE KING",
+  description: "40.000 PUFFS - Display LED",
+  price: 109.90,
+  image: "/ELFBAR40K.png",
+  category: "pod",
+  brand: "Elf Bar",
+  puffs: "40000",
+  inStock: true,
+  flavors: [
+    "Baja Splash",
+    "Blue Razz Ice",
+    "Cherry Fuse",
+    "Cherry Strazz",
+    "Dragon Strawnana",
+    "Mango Magic",
+    "Miami Mint",
+    "Peach+",
+    "Scary Berry",
+    "Sour Apple Ice",
+    "Sour Lush Gummy",
+    "Sour Straw Dragon",
+    "Summer Splash",
+    "Tiger’s Blood",
+    "Watermelon Ice"
+  ]
+},
+{
+    id: "elfbar-bateria",
+    name: "ELFBAR BATERIA",
+    description: "BATERIA ELFBAR KIT",
+    price: 59.90,
+    image: "/ELFBARBATERIA.png",
     category: "pod",
     brand: "Elf Bar",
-    puffs: "40000",
     inStock: true,
-    flavors: [/* mantido */]
+    flavors: ["Lemon Mint", "Strawberry Mango", "Blueberry Ice"]
+  },
+
+
+
+  // LOST MARY
+  {
+    id: "lostmary-20000",
+    name: "LOSTMARY 20K ",
+    description: "20.000 PUFFS",
+    price: 79.90,
+    image: "/LOSTMARY20K.png",
+    category: "pod",
+    brand: "Lost Mary",
+    puffs: "20000",
+    inStock: true,
+    flavors: ["Morango Melancia", "Uva Ice", "Limonada", "Blueberry", "Pêssego"]
+  },
+ {
+  id: "lostmary-25000",
+  name: "LOST MARY 25K",
+  description: "25.000 PUFFS",
+  price: 84.90,
+  image: "/LOSTMARY25K.png",
+  category: "pod",
+  brand: "Lost Mary",
+  puffs: "25000",
+  inStock: true,
+  flavors: [
+    "Baja Splash",
+    "Black Cherry Peach",
+    "Blue Razz Ice",
+    "Cranberry Apple",
+    "Miami Mint",
+    "Watermelon Ice",
+    "Tripple Berry",
+    "Strawberry Orange Pineapple",
+    "Sour Apple Grape Kiwi",
+    "Orange Mango Guava",
+    "Peach+",
+    "Grapefruit Kiwi Strawberry",
+    "Sour Blackberry Blueberry",
+    "Rainbow Sherbet",
+    "Sour Apple Ice"
+  ]
+},
+  {
+  id: "lostmary-30000",
+  name: "LOST MARY 30K",
+  description: "30.000 PUFFS",
+  price: 79.90,
+  image: "/LOSTMARY30K.png",
+  category: "pod",
+  brand: "Lost Mary",
+  puffs: "30000",
+  inStock: true,
+  flavors: [
+    "Apple Grape",
+    "Aloe Grape Sour Apple",
+    "Banana Cherry Ice",
+    "Blue Razz Ice",
+    "Blueberry Watermelon",
+    "Cherry Lemon",
+    "Grapefruit Lemon Lime",
+    "Grapefruit Green Tea",
+    "Mango Strawberry Ice",
+    "Miami Chill",
+    "Mix Mint",
+    "Orange Strawberry",
+    "Raspberry Pineapple",
+    "Red Raspberry Strawberry",
+    "Watermelon B Pop",
+    "Watermelon Ice"
+  ]
+},
+
+  // OXBAR
+ 
+
+  {
+    id: "oxbar-10k",
+    name: "OXBAR 10K",
+    description: "10.000 PUFFS",
+    price: 79.90,
+    image: "/OXBAR10K.png",
+    category: "pod",
+    brand: "Oxbar",
+    puffs: "10000",
+    inStock: true,
+    flavors: ["Fruit Paradise", "Mango Bery Lemonade", "Pink Burst Chew", "Razz Pineapple", "Splash Bros Lemonade", "Wap Drops","Watermelon Remix Ice", "Mighty Mint"]
+  },
+  
+  {
+    id: "oxbar-30k",
+    name: "OXBAR 30K",
+    description: "30.000 PUFFS",
+    price: 104.90,
+    image: "/OXBAR30k.png",
+    category: "pod",
+    brand: "Oxbar",
+    puffs: "30000",
+    inStock: true,
+    flavors: ["Blueberry Raspberry Lemon", "Mr Bull", "Frosty Mint","Peach Blue Razz", "Sour Apple Ice", "Sour Fruit Drops","Strawberry Fudge", "Strawberry Watermelon Dragonfruit", "Strawberry Watermelon Peach","Watermelon Gumdrop", "Watermelon Mint", "White Grape","Grand Purple", "Oxlove", "Strawberry Watermelon","Double Apple", "Raspberry Lemon", "Paradise Grape","Grape Peach", "Passion Kiwi", "Fanta Strawberry","Sour Mango","Blackcurrant Lemon Ice","Red Ice","Blue Raspberry Lemon","Fresh Mint" ]
+  },
+
+  // SEX ADDICT
+  {
+    id: "sex-addict-28k",
+    name: "SEX ADDICT 28K",
+    description: "28.000 PUFFS - Tela LED",
+    price: 99.90,
+    image: "/SEXADDICT28K.png",
+    category: "pod",
+    brand: "Sex Addict",
+    puffs: "28000",
+    inStock: true,
+    flavors: ["Banana Ice", "BlueBerry Ice", "Mirtilo Ice", "Grape Acai", "Grape Ice", "Grape Kiwi Ice", "Grape Watermelon Ice", "Green Apple Ice", "Ice Mint", "Kiwi Passion Fruit", "Kiwi Watermelon Ice", "Strawberry Banana", "Strawberry Grape Ice", "Strawberry Kiwi Ice", "Strawberry Watermelon","Watermelon Ice"]
   },
 
   // ADJUST
-  {
-    id: "adjust-40k",
-    name: "ADJUST 40K",
-    description: "40.000 PUFFS",
-    price: 104.90,
-    image: "/ADJUST40K.png",
-    category: "pod",
-    brand: "Adjust",
-    puffs: "40000",
-    inStock: true,
-    flavors: [/* mantido */]
-  },
+ {
+  id: "adjust-40k",
+  name: "ADJUST 40K",
+  description: "40.000 PUFFS",
+  price: 114.90,
+  image: "/ADJUST40K.png",
+  category: "pod",
+  brand: "Adjust",
+  puffs: "40000",
+  inStock: true,
+  flavors: [
+    "Black Razz Baja Splash",
+    "Blue Razz Ice",
+    "Midnight Ice Chill",
+    "Mixed Mint",
+    "Orange Strawberry",
+    "Peach",
+    "Pineapple Ice Peach",
+    "Strawberry Banana",
+    "Strawberry Mint",
+    "Candy Tangerine White Gummy",
+    "Triple Berry Grape",
+    "Watermelon B-Pop",
+    "Watermelon Mango Peach"
+  ]
+},
 
-  // NIKBAR
+  // FUNKYLANDS
+  {
+    id: "funkylands-7000",
+    name: "FUNKYLANDS 7K",
+    description: "7.000 PUFFS",
+    price: 54.90,
+    image: "/FUNKYLAND7K.png",
+    category: "pod",
+    brand: "Funkylands",
+    puffs: "7000",
+    inStock: true,
+    flavors: ["Rainbow Candy", "Cool Mint", "Strawberry Ice"]
+  },
+   // NIKBAR
   {
     id: "nikbar-40k",
     name: "NIKBAR 40K",
@@ -126,6 +371,7 @@ export const products: Product[] = [
     brand: "Nikbar",
     puffs: "40000",
     inStock: true,
-    flavors: [/* mantido */]
-  }
-];
+    flavors: ["Mint", "Strawberry", "Grape"]
+  },
+  ]
+ 
